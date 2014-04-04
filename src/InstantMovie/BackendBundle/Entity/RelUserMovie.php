3d777896d8e4de2d2c6188ec17233f1b60b9b570
FranAznar <fran.aznar@tuguuintel.com>
2014-04-04 02:40:53 -0700

    The Entitys relations with the correct name.

---
+++ src/InstantMovie/BackendBundle/Entity/RelUserMovie.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace InstantMovie\BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ref_user_movie
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Ref_user_movie
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="movie_id", type="integer")
+     */
+    private $movieId;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publication_date", type="datetime")
+     */
+    private $publicationDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Ref_user_movie
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set movieId
+     *
+     * @param integer $movieId
+     * @return Ref_user_movie
+     */
+    public function setMovieId($movieId)
+    {
+        $this->movieId = $movieId;
+
+        return $this;
+    }
+
+    /**
+     * Get movieId
+     *
+     * @return integer 
+     */
+    public function getMovieId()
+    {
+        return $this->movieId;
+    }
+
+    /**
+     * Set publicationDate
+     *
+     * @param \DateTime $publicationDate
+     * @return Ref_user_movie
+     */
+    public function setPublicationDate($publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate
+     *
+     * @return \DateTime 
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+}
